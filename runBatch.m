52f66839e2d1c103acb331e314e60c19d1c14e28
robinhowk <robin-howk@uiowa.edu>
2018-08-02 14:30:47 -0500

    reorganize how variable for a burst are initialize

--- runBatch.m
+++ runBatch.m
@@ -51,6 +51,14 @@ t1 = tic;
     % start timer
     t2 = tic;
     
+    %----------------------------------------------------------------------
+    % Initialize daily variables
+    % set datapath for mat files for this day
+    % create folders for storing results
+    % get list of all mat files for this day
+    % initialize counts for tracking daily totals
+    % create structure for storing data to be written to cdfs
+    %----------------------------------------------------------------------
     % set data path for current day
     dataPath = sprintf('data/mat/%04d/%02d/%02d', date.Year, date.Month, date.Day);
     
@@ -62,94 +70,100 @@ t1 = tic;
     
     % initilize struct for tracking daily totalts
     countsDay = initializeCounts(histEdges, defaultNum);
-    
-    [summaryFigDay, cdfFilename] = setupDay( iDate, paramstring, histEdges);
-    
-    
-    cdfData = setupCdfRecords(cdfInfoMaster);
     numRecords = 0;
     totalRecordsDay = 0;
-        
-        for iFile = 1:size(filelist,1)
-            % get file path for burst
-            filename = filelist(iFile).name;
-            datafilename = sprintf('%s/%s', dataPath, filename);
-            load(datafilename);
-            
-            % continue if in plasmapause interval
-            if find(timestamp <= ppIntervals(:,2) & (timestamp + seconds(6)) > ppIntervals(:,1))
-                % create imagefile
-                % create timestamp with one second precision to find freq
-                % limit
-                tempTime = datevec(timestamp);
-                tempTime(6) = floor(tempTime(6));
-                tempTime = datetime(tempTime);
-                freqLimit = fceLimits(find((fceTimes > tempTime), 1));
-                freqLimit = find((fspec > freqLimit), 1);
-                [figname, resultFilename, imagefile, imagefile1, fspec] = setupBurst(filename, datafilename, paramstring, figFolder, resultsFolder, freqLimit, delta_psd);
-                [ridges, bwRidges] = find_ridges(paramfilename, datafilename, imagefile1);
-                
-                % if ridges are found, continue
-                if sum(sum(bwRidges)) >  0
-                    ridgesZero = ridges - min(min(ridges));
-                    [spine, bwSpine] = center_of_mass(ridgesZero, 2, 2);
-                    [ chorusElements, tracedElements, chorusCount ] = traceBurst( bwSpine, spine, imagefile, fspec, tspec, mu1, ridgesZero, errorLogId, filename);
-                    
-                    % create figure
-                    showBurstFigure(tspec, fspec, delta_psd, imagefile1, ridges, spine, tracedElements, chorusElements, chorusCount, histEdges.sweeprates, figname);
-                    
-                    % if chorus are found, continue
-                    if chorusCount > 0
-                        if numRecords + chorusCount > numel(cdfData.chorusEpoch)
-                            numEntries = 1000;
-                            % reallocate array sizes, add room for 1000
-                            % more entries
-                            cdfData.chorusEpoch = [cdfData.chorusEpoch; int64(zeros(numEntries, cdfInfoMaster.Variables{1,2}(1)))];
-                            cdfData.frequency =  [cdfData.frequency; cdfInfoMaster.VariableAttributes.FILLVAL{4,2} * ones(numEntries, cdfInfoMaster.Variables{4,2}(1))];
-                            cdfData.psd =  [cdfData.psd; cdfInfoMaster.VariableAttributes.FILLVAL{5,2} * ones(numEntries, cdfInfoMaster.Variables{5,2}(1))];
-                            cdfData.sweeprates = [cdfData.sweeprates; cdfInfoMaster.VariableAttributes.FILLVAL{6,2} * ones(numEntries, cdfInfoMaster.Variables{6,2}(1))];
-                            cdfData.burst = [cdfData.burst; int32(zeros(numEntries, cdfInfoMaster.Variables{7,2}(1)))];
-                            cdfData.chorusIndex = [cdfData.chorusIndex; int32(zeros(numEntries, cdfInfoMaster.Variables{8,2}(1)))];
-                        end
-                        
-                        % getHistCounts
-                        burstCounts = getHistCounts(chorusElements, histEdges);
-                        countsDay.chorusAngles = countsDay.chorusAngles + burstCounts.chorusAngles;
-                        countsDay.sweeprates = countsDay.sweeprates + burstCounts.sweeprates;
-                        countsDay.hourlyTotals(timestamp.Hour + 1) = countsDay.hourlyTotals(timestamp.Hour + 1) + chorusCount;
-                            
-                        % add psd sums and sweeprates to array
-                        for i = 1:chorusCount
-                            countsDay.psdSums(totalRecordsDay + i) = chorusElements(i).psdSumLine;
-                            countsDay.sweepratesList(totalRecordsDay + i) = chorusElements(i).sweeprate;
-                        end
-                        
-                        [cdfData, numRecords] = updateCdfRecords(cdfData, chorusElements, timestamp, tspec, str2double(filename(13:15)), numRecords);
-                        
-                        % add to record count
-                        totalRecordsDay = totalRecordsDay + chorusCount;
-                        
-                        % save mat file
-                        save(resultFilename, 'imagefile', 'imagefile1', 'fspec', 'tspec', 'ridges', 'bwRidges', 'chorusElements', 'paramfilename', 'timestamp', 'tracedElements', 'spine', 'bwSpine');
-                    else
-                        % save mat file
-                        save(resultFilename, 'imagefile', 'imagefile1', 'fspec', 'tspec', 'ridges', 'bwRidges', 'paramfilename', 'timestamp');
-                    end
-                else
-                    % no ridges, save mat file
-                    save(resultFilename, 'imagefile', 'imagefile1', 'fspec', 'tspec', 'paramfilename', 'timestamp');
-                end
-            end % end of burst
-        end % end day
-        
-         % save summary data for day
-        resultsDayFilename = sprintf('%s/%04d%02d%02d_summary.mat', resultsFolder, iDate.Year, iDate.Month, iDate.Day);
-        save(resultsDayFilename, 'countsDay', 'totalRecordsDay');
+    
+    % setup cdf results
+    cdfData = setupCdfRecords(cdfInfoMaster);
+    
+    %----------------------------------------------------------------------
+    % move to end of day?
+    
+    [summaryFigDay, cdfFilename] = setupDay( iDate, paramstring, histEdges);
+    %----------------------------------------------------------------------
+    % process each file for day
+      for iFile = 1:size(filelist,1)
+        % get file path for burst
+        filename = filelist(iFile).name;
+        datafilename = sprintf('%s/%s', dataPath, filename);
+        load(datafilename);
+        %------------------------------------------------------------------
+        % check if current burst falls within a valid plasmapause interval. 
+        % If so, trim spectrogram, scale to 10log10 and continue processing 
+        % burst.
+        %------------------------------------------------------------------
+        if find(timestamp <= ppIntervals(:,2) & ...
+            (timestamp + seconds(6)) > ppIntervals(:,1))
+          % create filename for result and figure to be saved
+          figname = sprintf('%s/%s_%s.jpg', figFolder, strtok(filename, '.'), paramstring);
+          resultFilename = sprintf('%s/%s_result.mat', resultsFolder, strtok(filename, '.'));
+          % create spectrogram
+          [spect, fspec] = trimSpectrogram(timestamp, imagefile, fspec);
 
-        % save cdf file for day
-        if totalRecordsDay > 0
-            writeToCdf( cdfFilename, cdfDataMaster, cdfInfoMaster, cdfData, numRecords, tspec(1) );
-        end
+          % REMOVE RIDGE TRANSFORM AND ADD SNR MAPS
+          [ridges, bwRidges] = find_ridges(paramfilename, datafilename, spect);
+
+          % if ridges are found, continue
+          if sum(sum(bwRidges)) >  0
+            ridgesZero = ridges - min(min(ridges));
+            [spine, bwSpine] = center_of_mass(ridgesZero, 2, 2);
+            [ chorusElements, tracedElements, chorusCount ] = traceBurst( bwSpine, spine, imagefile, fspec, tspec, mu1, ridgesZero, errorLogId, filename);
+
+            % create figure
+            showBurstFigure(tspec, fspec, delta_psd, imagefile1, ridges, spine, tracedElements, chorusElements, chorusCount, histEdges.sweeprates, figname);
+
+            % if chorus are found, continue
+            if chorusCount > 0
+              if numRecords + chorusCount > numel(cdfData.chorusEpoch)
+                  numEntries = 1000;
+                  % reallocate array sizes, add room for 1000
+                  % more entries
+                  cdfData.chorusEpoch = [cdfData.chorusEpoch; int64(zeros(numEntries, cdfInfoMaster.Variables{1,2}(1)))];
+                  cdfData.frequency =  [cdfData.frequency; cdfInfoMaster.VariableAttributes.FILLVAL{4,2} * ones(numEntries, cdfInfoMaster.Variables{4,2}(1))];
+                  cdfData.psd =  [cdfData.psd; cdfInfoMaster.VariableAttributes.FILLVAL{5,2} * ones(numEntries, cdfInfoMaster.Variables{5,2}(1))];
+                  cdfData.sweeprates = [cdfData.sweeprates; cdfInfoMaster.VariableAttributes.FILLVAL{6,2} * ones(numEntries, cdfInfoMaster.Variables{6,2}(1))];
+                  cdfData.burst = [cdfData.burst; int32(zeros(numEntries, cdfInfoMaster.Variables{7,2}(1)))];
+                  cdfData.chorusIndex = [cdfData.chorusIndex; int32(zeros(numEntries, cdfInfoMaster.Variables{8,2}(1)))];
+              end
+
+              % getHistCounts
+              burstCounts = getHistCounts(chorusElements, histEdges);
+              countsDay.chorusAngles = countsDay.chorusAngles + burstCounts.chorusAngles;
+              countsDay.sweeprates = countsDay.sweeprates + burstCounts.sweeprates;
+              countsDay.hourlyTotals(timestamp.Hour + 1) = countsDay.hourlyTotals(timestamp.Hour + 1) + chorusCount;
+
+              % add psd sums and sweeprates to array
+              for i = 1:chorusCount
+                  countsDay.psdSums(totalRecordsDay + i) = chorusElements(i).psdSumLine;
+                  countsDay.sweepratesList(totalRecordsDay + i) = chorusElements(i).sweeprate;
+              end
+
+              [cdfData, numRecords] = updateCdfRecords(cdfData, chorusElements, timestamp, tspec, str2double(filename(13:15)), numRecords);
+
+              % add to record count
+              totalRecordsDay = totalRecordsDay + chorusCount;
+
+              % save mat file
+              save(resultFilename, 'imagefile', 'imagefile1', 'fspec', 'tspec', 'ridges', 'bwRidges', 'chorusElements', 'paramfilename', 'timestamp', 'tracedElements', 'spine', 'bwSpine');
+            else
+              % save mat file
+              save(resultFilename, 'imagefile', 'imagefile1', 'fspec', 'tspec', 'ridges', 'bwRidges', 'paramfilename', 'timestamp');
+            end
+          else
+            % no ridges, save mat file
+            save(resultFilename, 'imagefile', 'imagefile1', 'fspec', 'tspec', 'paramfilename', 'timestamp');
+          end
+        end % end of burst
+      end % end day
+
+       % save summary data for day
+      resultsDayFilename = sprintf('%s/%04d%02d%02d_summary.mat', resultsFolder, iDate.Year, iDate.Month, iDate.Day);
+      save(resultsDayFilename, 'countsDay', 'totalRecordsDay');
+
+      % save cdf file for day
+      if totalRecordsDay > 0
+          writeToCdf( cdfFilename, cdfDataMaster, cdfInfoMaster, cdfData, numRecords, tspec(1) );
+      end
 
         % create summary histograms for day
         showSummaryPanel(countsDay, histEdges, summaryFigDay);
@@ -258,4 +272,22 @@ function [resultsFolder, figFolder, cdfFolder] = createFolders(datapath, date, p
   if ~exist(cdfFolder, 'dir')
     mkdir(cdfFolder)
   end
+end
+
+function [spect, fspec] = trimSpectrogram(timestamp, imagefile, fspec)
+  % create timestamp with one second precision to find freq limit
+  tempTime = datevec(timestamp);
+  tempTime(6) = floor(tempTime(6));
+  tempTime = datetime(tempTime);
+  % find maximum frequency
+  freqLimit = fceLimits(find((fceTimes > tempTime), 1));
+  freqLimit = find((fspec > freqLimit), 1);
+
+  % trim psd to freq limit and scale to 10log10
+  if ~isempty(freqLimit)
+     imagefile = imagefile(1:freqLimit, :);
+     fspec = fspec(1:freqLimit);
+  end
+  
+  spect = 10*log10(imagefile);
 end
